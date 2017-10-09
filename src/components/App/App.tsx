903f1fbd278e6c8bd54eb9170a2497fbd0e6fee7
technotronic12 <elip@wix.com>
2017-10-10 01:46:43 +0300

    remove stupid container and make menu smaller instead, fix loader visibility

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { observer } from 'mobx-react';
-import { background, container } from './App.css';
+import { background } from './App.css';
 import { Menu } from '../Menu/Menu';
 import DevTools from 'mobx-react-devtools';
 import { Hero } from '../Row/Hero';
@@ -21,9 +21,7 @@ export class App extends React.Component<{ appStore: AppStore }, {}> {
         return (
             <div className={background}>
                 <Loader isLoaded={this.props.appStore.appIsLoaded}/>
-                <div className={container}>
-                    <Menu menuStore={this.props.appStore.menuStore}/>
-                </div>
+                <Menu menuStore={this.props.appStore.menuStore}/>
                 <Hero />
                 <DevTools />
             </div>
