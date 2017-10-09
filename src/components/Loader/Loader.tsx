1e1c9163991b0cef588f4cf2800ac93f81cb0fb2
technotronic12 <elip@wix.com>
2017-10-10 01:38:59 +0300

    delete ignored files, fix loader types

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,10 +1,9 @@
 import * as React from 'react';
 import { observer } from 'mobx-react';
 import * as style from './Loader.css';
-import { IAction } from 'mobx';
 
 @observer
-export class Loader extends React.Component< { isLoaded: IAction }, {}> {
+export class Loader extends React.Component< { isLoaded: () => Boolean }, {}> {
     render() {
         return (
             <div id={style.loaderWrapper} className={this.props.isLoaded ? style.fadeout : ''}>
