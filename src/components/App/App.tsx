d4318e5e5df06eaff984bc02a4c1069311e1accf
technotronic12 <elip@wix.com>
2017-10-10 01:31:35 +0300

    add hero and loader to app, use appStore instead if menuStore

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,18 +2,29 @@ import * as React from 'react';
 import { observer } from 'mobx-react';
 import { background, container } from './App.css';
 import { Menu } from '../Menu/Menu';
-import { MenuStore } from '../../models/MenuStore';
 import DevTools from 'mobx-react-devtools';
-
+import { Hero } from '../Row/Hero';
+import { Loader } from '../Loader/Loader';
+import { AppStore } from '../../models/AppStore';
 
 @observer
-export class App extends React.Component<{ menuStore: MenuStore }, {}> {
+export class App extends React.Component<{ appStore: AppStore }, {}> {
+    constructor() {
+        super();
+    }
+
+    componentDidMount() {
+        this.props.appStore.toggleLoaded();
+    }
+
     render() {
         return (
             <div className={background}>
+                <Loader isLoaded={this.props.appStore.appIsLoaded}/>
                 <div className={container}>
-                    <Menu menuStore={this.props.menuStore}/>
+                    <Menu menuStore={this.props.appStore.menuStore}/>
                 </div>
+                <Hero />
                 <DevTools />
             </div>
         );
