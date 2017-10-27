864fdaf67f7150206599a22d6575cc72964b7407
technotronic12 <elip@wix.com>
2017-10-27 18:27:52 +0300

    change image to me5, add typed text

--- src/components/Row/Hero.tsx
+++ src/components/Row/Hero.tsx
@@ -1,17 +1,40 @@
 import * as React from 'react';
 import * as style from './Hero.css';
+import Typist from 'react-typist';
 
 export class Hero extends React.Component {
     render() {
+        const initialDelayInMs = 1000;
+        const delayInMs = 700;
+        const sentences = [
+            'node developer.',
+            'Javascript developer.',
+            'React developer.',
+            'Scala developer.'
+        ];
+
         return (
-          <div>
-              <div className={`${style.hero} ${style.background}`}>
-                  <div className={[style.greeting].join(' ')}>
-                      <div className={[style.greetingText].join(' ')}>Hi, I am Eli. A Fullstack developer.</div>
-                      <div className={[style.travel].join(' ')}>
-                          Building things, from<span id="typed-strings"> home</span>.
-                      </div>
-                  </div>
+          <div className={`${style.hero} ${style.background}`}>
+              <div className={style.greeting}>
+                      <span className={style.greetingName}>Hi, I'm Eli.</span>
+                      <Typist>
+                          <Typist.Delay ms={initialDelayInMs}/>
+                          <span>I am a </span>
+                          {
+                              sentences.map((sentence, index) => {
+                                  return (
+                                    <span key={index}>
+                                      <span>{sentence}</span>
+                                      <Typist.Backspace count={sentence.length} delay={delayInMs}/>
+                                    </span>
+                                  )
+                              })
+                          }
+                          <span className={style.purple}>Fullstack developer</span><span>.</span>
+                          <br/>
+                          <span>I enjoy writing software for fun and profit.</span>
+
+                      </Typist>
               </div>
           </div>
         )
