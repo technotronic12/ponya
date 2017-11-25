25986c0f79aa47b4f198ca69daf49601af10ac3b
technotronic12 <elip@wix.com>
2017-11-25 21:47:46 +0200

    menu buttons should not control if their are displayed or not

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -5,17 +5,23 @@ import { menuStore } from '../../models/MenuStore';
 import { MenuButton } from './MenuButton/MenuButton';
 import { MenuItem } from './MenuItem/MenuItem';
 import * as style from './Menu.css';
+import * as animations from '../../animation.css';
 
 @observer
 export class Menu extends React.Component {
 
+  static openCloseClass() {
+    const openStyle = `${animations.fadein} ${style.open}`;
+    const closedStyle = `${animations.fadeout} ${style.closed}`;
+    return menuStore.isOpen() ? openStyle : closedStyle;
+  }
+
   render() {
     return (
-      <div className={`${style.menu} ` + (menuStore.open ? style.open : '')}>
+      <div className={`${style.menu}`}>
         <MenuButton onClick={menuStore.toggleOpen} isOpen={menuStore.open}/>
-        <div className={`${style.container}`}>
-          { _.map(menuStore.getItems(), item =>
-            <MenuItem key={item.index} isOpen={menuStore.isOpen()} icon={item.icon}/>)}
+        <div className={`${style.container} ${Menu.openCloseClass()}`}>
+          { _.map(menuStore.getItems(), item => <MenuItem key={item.index} icon={item.icon}/>) }
         </div>
       </div>
     );
--- src/components/Menu/MenuItem/MenuItem.tsx
+++ src/components/Menu/MenuItem/MenuItem.tsx
@@ -1,17 +1,12 @@
 import * as React from 'react';
 import * as style from './MenuItem.css';
-import * as animations from '../../../animation.css';
 
-export class MenuItem extends React.Component< { isOpen: boolean, icon: string }, {}> {
 
-  openCloseClass() {
-    const openStyles = `${animations.fadein} ${style.open}`;
-    return this.props.isOpen ? openStyles : `${animations.fadeout} ${style.closed}`;
-  }
+export class MenuItem extends React.Component< { icon: string }, {}> {
 
   render() {
     return (
-      <img src={this.props.icon} className={`${style.icon} ` + this.openCloseClass() }/>
+      <img src={this.props.icon} className={`${style.icon}`}/>
     );
   }
 }
