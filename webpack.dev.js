f9ffc78e404dcb96f0c2471f2cafc125c5d82a22
technotronic12 <elip@wix.com>
2018-04-11 23:47:38 +0300

    move mocha loader to dev, add compression plugin to prod

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,6 +5,14 @@ const common = require('./webpack.common.js');
 module.exports = merge(common, {
   devtool: 'inline-source-map',
   mode: 'development',
+  module: {
+    rules: [{
+      test: /\.spec\.tsx$/,
+      use: 'mocha-loader',
+      exclude: /node_modules/,
+    }
+    ]
+  },
   plugins: [
     new webpack.WatchIgnorePlugin([/\.js$/, /\.d\.ts$/])
   ]
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,15 +1,19 @@
-const webpack = require('webpack');
 const merge = require('webpack-merge');
-//const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
   plugins: [
-    //new UglifyJSPlugin(),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production')
+    new UglifyJSPlugin({ sourceMap: true }),
+    new CompressionPlugin({
+      asset: '[path].gz[query]',
+      algorithm: 'gzip',
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0.8
     })
-  ],
+  ]
 });
