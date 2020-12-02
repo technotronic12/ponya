5ca27f740085cb6105c32ad6b3b673554b5abe36
technotronic12 <elip@wix.com>
2020-12-02 18:45:50 +0200

    remove comments

--- src/components/Timeline/TimelineItem/TimelineItem.tsx
+++ src/components/Timeline/TimelineItem/TimelineItem.tsx
@@ -1,13 +1,8 @@
 import React from 'react';
-// import {timelineItem} from './TimelineItem.scss';
 import * as styles from './TimelineItem.scss';
 
 export class TimelineItem extends React.Component<{}, {}> {
-  // todo: TimelineItem should render children, Resume should use Timeline which renders resumeItem
-
   render() {
-    console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>', styles, styles.timelineItem);
-
     return (
       <div className={ styles.timelineItem }>
         { this.props.children }
