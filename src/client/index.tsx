badbcd37735cac1fcf1231108841b2d4b0a4232b
Eli Pony <eli@poyna.space>
2020-12-18 19:57:39 +0200

    dir structure client server

---
+++ src/client/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { App } from './components/App/App'
+import { appStore } from '../models/AppStore'
+
+ReactDOM.render(<App appStore={appStore} />, document.getElementById('root'))
--- src/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { App } from './components/App/App'
-import { appStore } from './models/AppStore'
-
-ReactDOM.render(<App appStore={appStore} />, document.getElementById('root'))
